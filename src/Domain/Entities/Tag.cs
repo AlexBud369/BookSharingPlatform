7eefa07b4533230f53f756dce463018174ea982b
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-02 17:22:47 +0300

    feat(domain): add Tag entity for book categorization
    - Implemented Tag entity with fields for id, name and book collection
    - Added private constructor for EF Core support and encapsulation
    - Included validation through private setter for TagName
    - Added UpdateTagName method for controlled modification

---
+++ src/Domain/Entities/Tag.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Domain.Entities
+{
+    /// <summary>
+    /// The entity of a tag is used to categorize books (genre, language)
+    /// </summary>
+    public class Tag
+    {
+        private Guid tagId;
+        private string tagName;
+        private List<Book> books;
+
+        private Tag() 
+        {
+            books = new List<Book>();
+        }
+
+        public Tag(Guid tagId, string tagName)
+        {
+            tagId = Guid.NewGuid();
+            this.tagName = tagName;
+            books = new List<Book>();
+        }
+
+        public Guid TagId
+        {
+            get { return tagId; }
+        }
+
+        public string TagName {  
+            get { return tagName; }
+            private set { tagName = value; }
+        }
+        public void UpdateTagName(string newTagName)
+        {
+            TagName = newTagName;
+        }
+
+        public List<Book> Books
+        {
+            get { return books; }
+        }
+    }
+}
