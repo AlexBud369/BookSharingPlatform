bae5a4a4e0e65a269885c0fcdf6640eda70137fc
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-03 23:04:07 +0300

    feat: add IBookRepository interface for book data operations (CRUD, search, filter, pagination)

---
+++ src/Domain/Repositories/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories;
+
+public interface IBookRepository
+{
+    Task<Book> GetByIdAsync(Guid bookId);
+    Task<IEnumerable<Book>> GetAllAsync(string? search, IEnumerable<string>? tags, Guid? createdByUserId, int page, int pageSize);
+    Task AddAsync(Book book);
+    Task UpdateAsync(Book book);
+    Task DeleteAsync(Guid bookId);
+}
+
