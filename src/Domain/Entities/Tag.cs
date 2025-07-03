958a92b4ad41cd054b36997ff6330298fb5e9be9
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-03 21:24:02 +0300

    fix: refactor Tag entity - replaced private fields with auto-properties - removed UpdateTagName method - removed redundant tagId parameter from constructor - used IReadOnlyList<Book> for Books with private backing List - removed validation from constructor

--- src/Domain/Entities/Tag.cs
+++ src/Domain/Entities/Tag.cs
@@ -5,46 +5,24 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
-namespace Domain.Entities
+namespace Domain.Entities;
+
+/// <summary>
+/// The entity of a tag is used to categorize books (genre, language)
+/// </summary>
+public class Tag
 {
-    /// <summary>
-    /// The entity of a tag is used to categorize books (genre, language)
-    /// </summary>
-    public class Tag
+    public Guid tagId { get; init; } = Guid.NewGuid();
+    public string tagName { get; private set; }
+    public IReadOnlyList<Book> Books { get => BooksList.AsReadOnly(); }
+    private readonly List<Book> BooksList = new();
+
+    private Tag() { }
+
+    public Tag(Guid tagId, string tagName)
     {
-        private Guid tagId;
-        private string tagName;
-        private List<Book> books;
-
-        private Tag() 
-        {
-            books = new List<Book>();
-        }
-
-        public Tag(Guid tagId, string tagName)
-        {
-            tagId = Guid.NewGuid();
-            this.tagName = tagName;
-            books = new List<Book>();
-        }
-
-        public Guid TagId
-        {
-            get { return tagId; }
-        }
-
-        public string TagName {  
-            get { return tagName; }
-            private set { tagName = value; }
-        }
-        public void UpdateTagName(string newTagName)
-        {
-            TagName = newTagName;
-        }
-
-        public List<Book> Books
-        {
-            get { return books; }
-        }
+        this.tagName = tagName;
     }
+
 }
+
