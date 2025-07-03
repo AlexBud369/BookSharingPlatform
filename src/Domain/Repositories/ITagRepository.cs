46224e8ce93ed17c6e0409abf617ee302b1d64a0
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-03 23:05:16 +0300

    feat: add ITagRepository interface for tag data operations

---
+++ src/Domain/Repositories/ITagRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories;
+
+public interface ITagRepository
+{
+    Task<Tag> GetByIdAsync(Guid tagId);
+    Task<IEnumerable<Tag>> GetAllAsync();
+    Task AddAsync(Tag tag);
+    Task UpdateAsync(Tag tag);
+    Task DeleteAsync(Guid tagId);
+}
