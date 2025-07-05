fd57d1a2fad7a2addc1e76fe8c4887cf075e48bc
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-05 18:19:42 +0300

    feat: add OnlyAdminAcsess exception

---
+++ src/Application/Common/Exceptions/AdminOnlyAccessException.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.Common.Exceptions;
+
+public class AdminOnlyAccessException : Exception
+{
+    public AdminOnlyAccessException() : base("Only admins can perfom this action") { }
+
+    public AdminOnlyAccessException(string message) : base(message) { }
+    public AdminOnlyAccessException(string message, Exception innerException)
+        : base(message, innerException) { }
+}
+
