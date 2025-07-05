963eff5c9f40fce69b2a8052d7be79a21cb7c9d5
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-05 18:05:36 +0300

    feat: add RefreshTokenNotFound custom exception

---
+++ src/Application/Common/Exceptions/RefreshTokenNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Common.Exceptions;
+
+public class RefreshTokenNotFoundException : Exception
+{
+    public RefreshTokenNotFoundException() { }
+
+    public RefreshTokenNotFoundException(string message) : base(message) { }
+    public RefreshTokenNotFoundException(string message, Exception innerException)
+        : base(message, innerException) { }
+
+
+}
