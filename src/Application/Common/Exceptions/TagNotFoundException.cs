1189545537bcfceaefcc82d1ffdb8fe2285f81f4
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-05 18:02:40 +0300

    feat: add TagNotFound custom exception

---
+++ src/Application/Common/Exceptions/TagNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Common.Exceptions;
+
+public class TagNotFoundException : Exception
+{
+    public TagNotFoundException() { }
+
+    public TagNotFoundException(string message) : base(message) { }
+    public TagNotFoundException(string message, Exception innerException)
+        : base(message, innerException) { }
+
+    public TagNotFoundException(Guid tagId)
+        : base($"Tag with Id {tagId} was not found") { }
+
+}
+
