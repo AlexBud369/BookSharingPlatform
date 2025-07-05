b7d15b384b8ed772a54e19e2649f1d0d71bd0985
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-05 17:56:29 +0300

    feat: add UserNotFound custom exception

---
+++ src/Application/Common/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Common.Exceptions;
+
+public class UserNotFoundException : Exception
+{
+    public UserNotFoundException() { }
+
+    public UserNotFoundException(string message) : base(message) { }
+    public UserNotFoundException(string message, Exception innerException)
+        :base(message, innerException) { }
+
+    public UserNotFoundException(Guid userId) 
+        : base($"User with Id {userId} was not found") { }
+
+}
+
