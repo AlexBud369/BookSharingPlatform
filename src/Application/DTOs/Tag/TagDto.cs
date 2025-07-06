be0acfa25f45acc249e8fd6bd075c9ebba3517ee
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-06 19:39:12 +0300

    feat: add Tag DTOs - TagDto: used for returning tag details in API responses, - TagCreateDto: used for creating new tags

---
+++ src/Application/DTOs/Tag/TagDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Tag;
+
+public class TagDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+}
