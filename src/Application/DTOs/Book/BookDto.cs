0b273b5448d521b3c01f07913a03a6c0931f5612
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-06 19:34:28 +0300

    feat: add Book DTOs - BookDto used for returning book details in API responses, includes all book properties and tag names - BookCreateDto: used for creating new books - BookUpdateDto: used for updating books

---
+++ src/Application/DTOs/Book/BookDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Book;
+
+public class BookDto
+{
+    public Guid Id { get; set; }
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string? Description { get; set; }
+    public string? CoverImageUrl { get; set; }
+    public Guid CreatedByUserId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public IEnumerable<string> Tags { get; set; }
+}
+
