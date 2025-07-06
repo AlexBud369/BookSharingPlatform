a4289b627af57a0a80da314c9fa3e4a5720f3b18
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-06 19:56:46 +0300

    feat: add RefreshTokenDto -  Used for returning refresh token details in API responses

---
+++ src/Application/DTOs/RefreshTokenDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs;
+
+public class RefreshTokenDto
+{
+    public Guid Id { get; set; }
+    public string Token { get; set; }
+    public Guid UserId { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime ExpiresAt { get; set; }
+    public bool IsRevoked { get; set; }
+}
