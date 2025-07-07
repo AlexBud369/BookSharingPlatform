4e57559bfbe438fc83663155bb02298e335631e5
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-07 13:57:45 +0300

    feat: add Tag profile with naming conventions - Maps tagId->Id and tagName->Name - Handles CreateDto with private setters

---
+++ src/Application/Common/Mappings/TagMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Application.DTOs.Tag;
+
+namespace Application.Common.Mappings;
+
+public class TagMappingProfile : Profile
+{
+    public TagMappingProfile()
+    {
+        CreateMap<Tag, TagDto>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.tagId))
+            .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.tagName));
+
+        CreateMap<TagCreateDto, Tag>()
+            .ConvertUsing(src => new Tag(Guid.NewGuid(), src.Name));
+    }
+}
