1d7bca5bcb67b8da510dc6d06093c3369751ed3a
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-07 13:55:21 +0300

    feat: add User profile with auth support - Maps RegisterDto with private setters - Handles userId->Id conversions - Removed LoginDto to User mapping as it is not used (handled in service)

---
+++ src/Application/Common/Mappings/UserMappingProfile.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Application.DTOs.User;
+
+namespace Application.Common.Mappings;
+
+public class UserMappingProfile : Profile
+{
+    public UserMappingProfile() 
+    {
+        CreateMap<User, UserDto>()
+            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.userId));
+        CreateMap<RegisterDto, User>()
+            .ConvertUsing(src => new User(src.Username, src.Password, src.Email));
+
+        CreateMap<LoginDto, User>()
+            .ForMember(dest => dest.userId, opt => opt.Ignore())
+            .ForMember(dest => dest.Username, opt => opt.Ignore())
+            .ForMember(dest => dest.PasswordHash, opt => opt.Ignore())
+            .ForMember(dest => dest.Role, opt => opt.Ignore())
+            .ForMember(dest => dest.IsBlocked, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore());
+    }
+
+}
