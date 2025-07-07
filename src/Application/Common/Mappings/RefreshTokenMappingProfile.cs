76b7992819b763e0c205089c9c2640adb9295cec
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-07 13:58:45 +0300

    feat: add RefreshToken profile - Auto-maps identical properties - ConvertUsing for DTO with private setters

---
+++ src/Application/Common/Mappings/RefreshTokenMappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Application.DTOs;
+
+namespace Application.Common.Mappings;
+
+public class RefreshTokenMappingProfile : Profile
+{
+    public RefreshTokenMappingProfile()
+    {
+        CreateMap<RefreshToken, RefreshTokenDto>();
+        CreateMap<RefreshTokenDto, RefreshToken>()
+            .ConvertUsing(src => new RefreshToken(src.Token, src.UserId, src.ExpiresAt));
+    }
+}
