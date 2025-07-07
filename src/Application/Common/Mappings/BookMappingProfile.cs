5f5a694e5713fc5091114639ba6210ef3429f246
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-07 13:52:49 +0300

    feat: add Book profile with custom converters - Handles Book<->DTO conversions with private setters - Special mapping for Tags collection - UpdateDto ignores props for service methods

---
+++ src/Application/Common/Mappings/BookMappingProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Application.DTOs;
+
+namespace Application.Common.Mappings;
+
+public class BookMappingProfile : Profile
+{
+    public BookMappingProfile()
+    {
+        CreateMap<Book, BookDto>()
+            .ForMember(dest => dest.Tags, opt => opt.MapFrom(src => src.Tags.Select(t => t.tagName)));
+
+        CreateMap<BookCreateDto, Book>()
+            .ConvertUsing(src => new Book(src.Title, src.Author, Guid.Empty, src.Description));
+
+        ///< summary >
+        /// All properties are ignored (Ignore) since updates are performed
+        /// via dedicated methods (UpdateTitle, UpdateAuthor, etc.) in the Services 
+        /// </summary>
+        CreateMap<BookUpdateDto, Book>()
+            .ForMember(dest => dest.Title, opt => opt.Ignore())
+            .ForMember(dest => dest.Author, opt => opt.Ignore())
+            .ForMember(dest => dest.Description, opt => opt.Ignore())
+            .ForMember(dest => dest.Tags, opt => opt.Ignore())
+            .ForMember(dest => dest.Id, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedByUserId, opt => opt.Ignore())
+            .ForMember(dest => dest.CreatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.CoverImageUrl, opt => opt.Ignore());
+
+    }
+}
