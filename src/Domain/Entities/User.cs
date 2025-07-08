c11715ceb21c54de1a81cbd5382120c9ea02c939
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-08 18:54:29 +0300

    fix: remove RefreshToken entity and Role enum - removed RefreshToken entity as ASP.NET Core Identity will handle refresh tokens - removed Role enum in preparation for using IdentityRole<Guid> - updated User entity to remove RefreshTokens and Role properties temporarily

--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -1,18 +1,17 @@
-﻿using System.Data;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Domain.Entities;
+
 public class User
 {
     public Guid userId { get; init; } = Guid.NewGuid();
-    public string Username { get; set; }
-    public string Email { get; set; }
-    public string PasswordHash { get; set; }
-    public Role Role { get; set; }
-    public bool IsBlocked { get; set; } = false;
+    public string Username { get; private set; }
+    public string Email { get; private set; }
+    public string PasswordHash { get; private set; }
+    public bool IsBlocked { get; private set; } = false;
     public DateTime CreatedAt { get; } = DateTime.UtcNow;
-    public IReadOnlyList<RefreshToken> RefreshTokens { get => RefreshTokensList.AsReadOnly(); }
     public IReadOnlyList<Book> Books { get => BooksList.AsReadOnly(); }
-    private readonly List<RefreshToken> RefreshTokensList = new();
     private readonly List<Book> BooksList = new();
 
     private User() { }
--- src/Domain/Enums/Role.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Domain.Enums;
-
-public enum Role
-{
-    User = 1,
-    Admin = 2
-}
-
