75d8353b7a67f6398ebdd645059837b6de471dbc
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-08 18:59:17 +0300

    feat: add ApplicationRole for ASP.NET Core Identity - Added ApplicationRole class inheriting from IdentityRole<Guid> to support role-based authorization - Included private parameterless constructor for EF Core compatibility

---
+++ src/Domain/Entities/ApplicationRole.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Domain.Entities;
+
+public class ApplicationRole : IdentityRole<Guid>
+{
+
+    public ApplicationRole(string roleName) : base(roleName) { }
+
+    private ApplicationRole() { }
+}
