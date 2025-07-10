fae8d593a38202abdd0b2150f8a37b99df0b27b5
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-10 15:23:56 +0300

    feat: add AppDbContext for EF Core database configuration - Implements IdentityDbContext for user and role management - Defines many-to-many relationship between Book and Tag - Defines one-to-many relationship between Book and ApplicationUser

---
+++ src/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,44 @@
+﻿using Azure;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+public class AppDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
+{
+    public DbSet<Book> Books { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Entity<Book>()
+            .HasMany(b => b.Tags)
+            .WithMany(t => t.Books)
+            .UsingEntity(j => j.ToTable("BookTags"));
+
+        builder.Entity<Book>()
+            .HasOne(b => b.CreatedByUser)
+            .WithMany(u => u.Books)
+            .HasForeignKey(b => b.CreatedByUserId);
+
+        builder.Entity<Book>()
+            .Property(b => b.Title)
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Entity<Book>()
+            .Property(b => b.Author)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Entity<Tag>()
+            .Property(t => t.tagName)
+            .IsRequired()
+            .HasMaxLength(50);
+    }
+}
+}
