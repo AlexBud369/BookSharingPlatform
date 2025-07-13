f1dd09be611005ac15ede9edf7b2ff660c5cfd75
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-13 16:10:42 +0300

    feat: add command for user registration

---
+++ src/Application/Features/Auth/Commands/RegisterCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auth.Commands;
+
+public class RegisterCommandHandler : IRequestHandler<RegisterCommand, UserDto>
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly IMapper _mapper;
+
+    public RegisterCommand(UserManager<ApplicationUser> userManager, IMapper mapper)
+    {
+        _userManager = userManager;
+        _mapper = mapper;
+    }
+
+    public async Task<UserDto> Handle(RegisterCommand request, CancellationToken cancellationToken)
+    {
+        var user = new ApplicationUser
+        {
+            Email = request.Email,
+            UserName = request.UserName
+        };
+
+        var result = await _userManager.CreateAsync(user, request.Password);
+        if (!result.Succeeded)
+        {
+            var errors = string.Join("; ", result.Errors.Select(e => e.Description));
+            throw new UnauthorizedAccessException($"Registration failed: {errors}");
+        }
+
+        return _mapper.Map<UserDto>(user);
+    }
+}
