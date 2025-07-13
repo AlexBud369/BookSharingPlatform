a411250290a19c0da63cdb9b5646e9071ebf9e21
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-13 16:07:03 +0300

    feat: add GetAllUsersQuery for viewing all users

---
+++ src/Application/Features/Users/Queries/GetAllUsersQuery.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Application.DTOs.User;
+
+namespace Application.Features.Users.Queries;
+
+public class GetAllUsersQuery : IRequest<IEnumerable<UserDto>>
+{
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public string? Email { get; set; }
+    public string? UserName { get; set; }
+    public bool? IsBlocked { get; set; }
+}
---
+++ src/Application/Features/Users/Queries/GetAllUsersQueryHandler.cs
@@ -0,0 +1,46 @@
+﻿using Application.Common.Exceptions;
+using Application.DTOs.User;
+using AutoMapper;
+using Domain.Entities;
+using Infrastructure.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Application.Features.Users.Queries;
+
+public class GetAllUsersQueryHandler : IRequestHandler<GetAllUsersQuery, IEnumerable<UserDto>>
+{
+    private readonly AppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetAllUsersQueryHandler(AppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    public async Task<IEnumerable<UserDto>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+    {
+        var query = _context.Users.AsQueryable();
+
+        if (!string.IsNullOrEmpty(request.Email))
+        {
+            query = query.Where(u => u.Email.Contains(request.Email));
+        }
+        if (!string.IsNullOrEmpty(request.UserName))
+        {
+            query = query.Where(u => u.UserName.Contains(request.UserName));
+        }
+        if (request.IsBlocked.HasValue)
+        {
+            query = query.Where(u => u.IsBlocked == request.IsBlocked.Value);
+        }
+
+        var users = await query
+            .Skip((request.PageNumber - 1) * request.PageSize)
+            .Take(request.PageSize)
+            .ToListAsync(cancellationToken);
+
+        return _mapper.Map<IEnumerable<UserDto>>(users);
+    }
+}
