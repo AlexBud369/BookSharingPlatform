d36a386b1735fa522b42634f4ba8b1e355a3bca6
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-13 16:08:57 +0300

    feat: add a command to retrieve a user by ID

---
+++ src/Application/Features/Users/Queries/GetUserByIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Application.DTOs.User;
+
+namespace Application.Features.Users.Queries;
+
+public class GetUserByIdQuery : IRequest<UserDto>
+{
+    public Guid Id { get; set; }
+}
---
+++ src/Application/Features/Users/Queries/GetUserByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common.Exceptions;
+using Application.DTOs.User;
+using AutoMapper;
+using Infrastructure.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Application.Features.Users.Queries;
+
+public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, UserDto>
+{
+    private readonly AppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetUserByIdQueryHandler(AppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    public async Task<UserDto> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+    {
+        var user = await _context.Users
+            .FirstOrDefaultAsync(u => u.Id == request.Id, cancellationToken);
+
+        if (user == null)
+        {
+            throw new UserNotFoundException(request.Id);
+        }
+
+        return _mapper.Map<UserDto>(user);
+    }
+}
