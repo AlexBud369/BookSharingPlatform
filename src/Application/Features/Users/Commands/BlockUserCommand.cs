71c70b19bd203a0b99feb126bc50b1d20aa16981
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-13 15:57:14 +0300

    feat: add BlockUserCommand to allow admins to block users

---
+++ src/Application/Features/Users/Commands/BlockUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Application.Features.Users.Commands;
+
+public class BlockUserCommand : IRequest
+{
+    public Guid UserId { get; set; }
+    public Guid AdminId { get; set; }
+}
---
+++ src/Application/Features/Users/Commands/BlockUserCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Application.Common.Exceptions;
+using Domain.Entities;
+
+namespace Application.Features.Users.Commands;
+
+public class BlockUserCommandHandler : IRequestHandler<BlockUserCommand>
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public BlockUserCommandHandler(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+    public async Task Handle(BlockUserCommand request, CancellationToken cancellationToken)
+    {
+        var admin = await _userManager.FindByIdAsync(request.AdminId.ToString());
+        if (admin == null)
+        {
+            throw new UserNotFoundException(request.AdminId);
+        }
+
+        if (!await _userManager.IsInRoleAsync(admin, "Admin")) 
+        {
+            throw new AdminOnlyAccessException("Only admins can block users");
+        }
+
+        var user = await _userManager.FindByIdAsync(request.UserId.ToString());
+        if (user == null)
+        {
+            throw new UserNotFoundException(request.UserId);
+        }
+
+        user.IsBlocked = true;
+        var result = await _userManager.UpdateAsync(user);
+        if (!result.Succeeded)
+        {
+            throw new UnauthorizedAccessException("Failed to block user");
+        }
+    }
+}
