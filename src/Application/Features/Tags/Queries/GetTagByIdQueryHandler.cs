706c85f07c3cd8aceac4f13e12fcf1bced4d4a2a
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-14 12:30:27 +0300

    feat: add query to get tag by ID

---
+++ src/Application/Features/Tags/Queries/GetTagByIdQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Common.Exceptions;
+using Application.DTOs.Tag;
+using AutoMapper;
+using Infrastructure.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Application.Features.Tags.Queries;
+
+public class GetTagByIdQueryHandler : IRequestHandler<GetTagByIdQuery, TagDto>
+{
+    private readonly AppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetTagByIdQueryHandler(AppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    public async Task<TagDto> Handle(GetTagByIdQuery request, CancellationToken cancellationToken)
+    {
+        var tag = await _context.Tags
+            .FirstOrDefaultAsync(t => t.tagId == request.Id, cancellationToken);
+        if (tag == null)
+        {
+            throw new TagNotFoundException(request.Id);
+        }
+        return _mapper.Map<TagDto>(tag);
+    }
+}
