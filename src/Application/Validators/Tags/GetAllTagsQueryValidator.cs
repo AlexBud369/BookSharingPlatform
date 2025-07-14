b02906f9c1f42daf25f04b12c4fbb0d66af4ace5
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-14 16:24:26 +0300

    feat: add validators for Tags queries - Implement GetAllTagsQueryValidator with pagination and tag name validation: - Implement GetTagByIdQueryValidator with required ID check

---
+++ src/Application/Validators/Tags/GetAllTagsQueryValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using Application.Features.Tags.Queries;
+
+namespace Application.Validators.Tags;
+
+public class GetAllTagsQueryValidator : AbstractValidator<GetAllTagsQuery>
+{
+    public GetAllTagsQueryValidator()
+    {
+        RuleFor(x => x.PageNumber)
+            .GreaterThan(0).WithMessage("Page number must be greater than 0");
+
+        RuleFor(x => x.PageSize)
+            .InclusiveBetween(1, 100).WithMessage("Page size must be between 1 and 100");
+
+        RuleFor(x => x.TagName)
+            .MaximumLength(50).WithMessage("Tag name must not exceed 50 characters")
+            .Matches(@"^[a-zA-Z0-9\s-]*$")
+            .WithMessage("Tag name can only contain letters, numbers, spaces, and hyphens")
+            .When(x => !string.IsNullOrEmpty(x.TagName));
+    }
+}
---
+++ src/Application/Validators/Tags/GetTagByIdQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Application.Features.Tags.Queries;
+
+namespace Application.Validators.Tags;
+
+public class GetTagByIdQueryValidator : AbstractValidator<GetTagByIdQuery>
+{
+    public GetTagByIdQueryValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Tag ID is required");
+    }
+}
