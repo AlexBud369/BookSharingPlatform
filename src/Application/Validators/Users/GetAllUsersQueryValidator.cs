fb565f71edca275d9a6801e20b9d1a729f0be168
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-14 15:51:11 +0300

    feat: add validators for User queries - Implements GetUserByIdQueryValidator to validate user ID - Implements GetAllUsersQueryValidator to ensure valid pagination and search parameters

---
+++ src/Application/Validators/Users/GetAllUsersQueryValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using Application.Features.Users.Queries;
+
+namespace Application.Validators.Users;
+
+public class GetAllUsersQueryValidator : AbstractValidator<GetAllUsersQuery>
+{
+    public GetAllUsersQueryValidator()
+    {
+        RuleFor(x => x.PageNumber)
+            .GreaterThan(0).WithMessage("Page number must be greater than 0.");
+
+        RuleFor(x => x.PageSize)
+            .InclusiveBetween(1, 100).WithMessage("Page size must be between 1 and 100.");
+
+        RuleFor(x => x.Email)
+            .MaximumLength(100).WithMessage("Email must not exceed 100 characters.")
+            .When(x => !string.IsNullOrEmpty(x.Email));
+
+        RuleFor(x => x.UserName)
+            .MaximumLength(50).WithMessage("Username must not exceed 50 characters.")
+            .When(x => !string.IsNullOrEmpty(x.UserName));
+    }
+}
---
+++ src/Application/Validators/Users/GetUserByIdQueryValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using Application.Features.Users.Queries;
+
+namespace Application.Validators.Users;
+
+public class GetUserByIdQueryValidator : AbstractValidator<GetUserByIdQuery>
+{
+    public GetUserByIdQueryValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("User ID is required.");
+    }
+}
