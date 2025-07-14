7dec55d86a13881369d7dd39167fa30eaccd37f6
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-14 15:40:27 +0300

    feat: add command to allow admins to delete users

---
+++ src/Application/Features/Users/Commands/DeleteUserCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Exceptions;
+using Domain.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Features.Users.Commands;
+
+public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand>
+{
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public DeleteUserCommandHandler(UserManager<ApplicationUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+    {
+        var admin = await _userManager.FindByIdAsync(request.AdminId.ToString());
+        if (admin == null)
+        {
+            throw new UserNotFoundException(request.AdminId);
+        }
+
+        if (!await _userManager.IsInRoleAsync(admin, "Admin"))
+        {
+            throw new AdminOnlyAccessException("Only admins can delete users");
+        }
+
+        var user = await _userManager.FindByIdAsync(request.UserId.ToString());
+        if (user == null)
+        {
+            throw new UserNotFoundException(request.UserId);
+        }
+
+        var result = await _userManager.DeleteAsync(user);
+        if (!result.Succeeded)
+        {
+            throw new Exception($"Failed to delete user: {string.Join(", ", result.Errors.Select(e => e.Description))}");
+        }
+    }
+}
