b52d5d37919610acf7a8c9fcd6c464b414acdaa9
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-14 12:29:42 +0300

    feat: add query for viewing all tags

---
+++ src/Application/Features/Tags/Queries/GetAllTagsQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Application.DTOs.Tag;
+
+namespace Application.Features.Tags.Queries;
+
+public class GetAllTagsQuery : IRequest<IEnumerable<TagDto>>
+{
+    public int PageNumber { get; set; } = 1;
+    public int PageSize { get; set; } = 10;
+    public string? TagName { get; set; }
+}
---
+++ src/Application/Features/Tags/Queries/GetAllTagsQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Application.DTOs.Tag;
+using AutoMapper;
+using Infrastructure.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Application.Features.Tags.Queries;
+
+public class GetAllTagsQueryHandler : IRequestHandler<GetAllTagsQuery, IEnumerable<TagDto>>
+{
+    private readonly AppDbContext _context;
+    private readonly IMapper _mapper;
+
+    public GetAllTagsQueryHandler(AppDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    public async Task<IEnumerable<TagDto>> Handle(GetAllTagsQuery request, CancellationToken cancellationToken)
+    {
+        var query = _context.Tags.AsQueryable();
+
+        if (!string.IsNullOrEmpty(request.TagName))
+            query = query.Where(t => t.tagName.Contains(request.TagName));
+
+        var tags = await query
+            .Skip((request.PageNumber - 1) * request.PageSize)
+            .Take(request.PageSize)
+            .ToListAsync(cancellationToken);
+
+        return _mapper.Map<IEnumerable<TagDto>>(tags);
+    }
+}
