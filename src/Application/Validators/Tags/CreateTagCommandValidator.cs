9cfc2597df9de8add39a7e7a4b37581347eef0f2
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-14 16:25:15 +0300

    feat: add validation for Tag creating command

---
+++ src/Application/Validators/Tags/CreateTagCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Application.Features.Tags.Commands;
+
+namespace Application.Validators.Tags;
+
+public class CreateTagCommandValidator : AbstractValidator<CreateTagCommand>
+{
+    public CreateTagCommandValidator()
+    {
+        RuleFor(x => x.tagName)
+            .NotEmpty().WithMessage("Tag name is required")
+            .MaximumLength(50).WithMessage("Tag name must not exceed 50 characters");
+    }
+}
