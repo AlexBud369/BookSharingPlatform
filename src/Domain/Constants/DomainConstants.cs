518c202d68b3be5cde6a5c359943ec54bd60baae
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-17 19:19:33 +0300

    feat: add DomainConstants file to manage constants

---
+++ src/Domain/Constants/DomainConstants.cs
@@ -0,0 +1,15 @@
+﻿namespace Domain.Constants;
+
+public static class DomainConstants
+{
+    public static class Book
+    {
+        public const int TitleMaxLength = 200;
+        public const int AuthorMaxLength = 100;
+    }
+
+    public static class Tag
+    {
+        public const int NameMaxLength = 50;
+    }
+}
