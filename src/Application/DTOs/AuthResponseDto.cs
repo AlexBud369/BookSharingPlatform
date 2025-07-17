212ce991a52848f2163fa585cd4194d2193aecdc
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-17 13:05:33 +0300

    feat: Add UserDto to AuthResponseDto to include user information in auth responses

--- src/Application/DTOs/AuthResponseDto.cs
+++ src/Application/DTOs/AuthResponseDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Application.DTOs.User;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,4 +11,5 @@ public class AuthResponseDto
 {
     public string AccessToken { get; set; }
     public string RefreshToken { get; set; }
+    public UserDto User { get; set; }
 }
