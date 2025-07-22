506225de435058c3c7296aa3cf5a8e32a8536f05
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-23 00:10:12 +0300

    feat: add LogoutCommandValidator

---
+++ src/Application/Validators/Auth/LogoutCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Constants;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+using Application.Features.Auth.Commands;
+
+namespace Application.Validators.Auth;
+
+public class LogoutCommandValidator : AbstractValidator<LogoutCommand>
+{
+    public LogoutCommandValidator(IStringLocalizer<SharedResource> localizer)
+    {
+        RuleFor(x => x.UserId)
+            .NotEmpty().WithMessage(localizer["UserIdRequired"]);
+
+        RuleFor(x => x.RefreshToken)
+            .NotEmpty().WithMessage(localizer["RefreshTokenRequired"])
+            .MaximumLength(DomainConstants.User.RefreshTokenMaxLength).WithMessage(localizer["RefreshTokenTooLong"]);
+    }
+}
