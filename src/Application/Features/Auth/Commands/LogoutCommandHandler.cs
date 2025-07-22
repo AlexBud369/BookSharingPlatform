7a19aadb2366719ebeceed7b72a7c1592f2593ac
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-22 15:02:55 +0300

    feat: add LogoutCommand and Handler for user logout - Created LogoutCommand to specify user ID and refresh token for logout - Implemented LogoutCommandHandler to mark refresh token as revoked

---
+++ src/Application/Features/Auth/Commands/LogoutCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using Application.Common;
+using Domain.Entities;
+using Infrastructure.Data;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Localization;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Features.Auth.Commands;
+
+public class LogoutCommandHandler : IRequestHandler<LogoutCommand>
+{
+    private readonly AppDbContext _context;
+    private readonly IStringLocalizer<SharedResource> _localizer;
+
+    public LogoutCommandHandler(AppDbContext context, IStringLocalizer<SharedResource> localizer)
+    {
+        _context = context;
+        _localizer = localizer;
+        Guard.Initialize(_localizer);
+    }
+
+    public async Task Handle(LogoutCommand request, CancellationToken cancellationToken)
+    {
+        var refreshToken = await _context.RefreshTokens
+            .FirstOrDefaultAsync(
+            rt => rt.UserId == request.UserId
+            && rt.Token == request.RefreshToken
+            && !rt.IsRevoked, cancellationToken);
+
+        Guard.AgainstNull(refreshToken, nameof(request.RefreshToken), "RefreshTokenNotFound");
+
+        refreshToken.IsRevoked = true;
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
