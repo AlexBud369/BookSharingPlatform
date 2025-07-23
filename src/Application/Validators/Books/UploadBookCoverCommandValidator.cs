19fb322219aca46547e84f0b549f1522539088fc
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-23 17:44:32 +0300

    refactor: update UploadBookCoverCommandValidator for IFormFile - Replaced CoverImageUrl validation with CoverImage (IFormFile) validation - Added checks for file presence, size, and allowed extensions (.jpg, .jpeg, .png) - Removed URL-specific validation (NotEmpty, MaximumLength, Matches)

--- src/Application/Validators/Books/UploadBookCoverCommandValidator.cs
+++ src/Application/Validators/Books/UploadBookCoverCommandValidator.cs
@@ -11,14 +11,17 @@ public class UploadBookCoverCommandValidator : AbstractValidator<UploadBookCover
     public UploadBookCoverCommandValidator(IStringLocalizer<SharedResource> localizer)
     {
         RuleFor(x => x.Id)
-            .NotEmpty().WithMessage(localizer["BookIdRequired"]);
+           .NotEmpty().WithMessage(localizer["BookIdRequired"]);
 
         RuleFor(x => x.UserId)
             .NotEmpty().WithMessage(localizer["UserIdRequired"]);
 
-        RuleFor(x => x.CoverImageUrl)
-            .NotEmpty().WithMessage(localizer["CoverImageUrlRequired"])
-            .MaximumLength(DomainConstants.BookCover.UrlMaxLength).WithMessage(localizer["CoverImageUrlTooLong"])
-            .Matches(@"^https?://").WithMessage(localizer["InvalidCoverImageUrl"]);
+        RuleFor(x => x.CoverImage)
+            .NotNull().WithMessage(localizer["CoverImageRequired"])
+            .Must(file => file.Length > 0).WithMessage(localizer["CoverImageRequired"])
+            .Must(file => file.Length <= DomainConstants.BookCover.MaxFileSizeBytes)
+            .WithMessage(localizer["CoverImageTooLarge", DomainConstants.BookCover.MaxFileSizeBytes / 1024 / 1024])
+            .Must(file => new[] { ".jpg", ".jpeg", ".png" }.Contains(Path.GetExtension(file.FileName).ToLowerInvariant()))
+            .WithMessage(localizer["InvalidCoverImageFormat"]);
     }
 }
