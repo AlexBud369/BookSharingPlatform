db2f33b4289425ef05f8c912174e46bb222fde7b
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-28 20:27:40 +0300

    feat: Redesign UserRole enum with proper serialization

---
+++ src/Domain/Enums/UserRole.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace Domain.Enums;
+
+public enum UserRole
+{
+    [EnumMember(Value = "User")]
+    User = 0,
+    [EnumMember(Value = "Admin")]
+    Admin = 1
+}
