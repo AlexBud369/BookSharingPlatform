d8b8ffac7dfe26423bae7ad0709b3148f8bb4518
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-07-31 15:39:37 +0300

    fix: add missing semicolon in Book.cs

--- src/Domain/Entities/Book.cs
+++ src/Domain/Entities/Book.cs
@@ -9,7 +9,7 @@ public class Book
     public string Author { get; set; } = string.Empty;
     public string? Description { get; set; }
     public string? CoverImageUrl { get; set; }
-    public Guid CreatedByUserId { get; set }
+    public Guid CreatedByUserId { get; set; }
     public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
     public IReadOnlyList<Tag> Tags { get => TagsList.AsReadOnly(); }
     private readonly List<Tag> TagsList = new();
