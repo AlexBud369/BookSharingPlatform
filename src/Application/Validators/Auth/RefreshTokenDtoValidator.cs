2652028ded9247b571833aad21970b020d6d9024
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-08-01 17:16:53 +0300

    feat: add validators for authentication DTOs - Implemented RegisterDtoValidator, LoginDtoValidator, and RefreshTokenDtoValidator using FluentValidation

---
+++ src/Application/Validators/Auth/RefreshTokenDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using Application.Common;
+using Application.DTOs;
+using Domain.Constants;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+
+namespace Application.Validators.Auth;
+
+public class RefreshTokenDtoValidator : AbstractValidator<RefreshTokenDto>
+{
+    public RefreshTokenDtoValidator(IStringLocalizer<SharedResources> localizer)
+    {
+        RuleFor(x => x.Token)
+            .NotEmpty().WithMessage(localizer.GetString(SharedResources.RefreshTokenRequired))
+            .MaximumLength(DomainConstants.User.RefreshTokenMaxLength).WithMessage(localizer.GetString(SharedResources.RefreshTokenTooLong));
+    }
+}
