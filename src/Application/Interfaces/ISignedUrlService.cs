7acaeedcc4fa3e2d54de749dda765e62445e357f
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-08-01 17:18:03 +0300

    feat: Add ISignedUrlService and SignedUrlService for S3 signed URLs

---
+++ src/Application/Interfaces/ISignedUrlService.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Interfaces;
+
+public interface ISignedUrlService
+{
+    Task<string> GenerateSignedUrlAsync(string fileName, int expiresInSeconds, CancellationToken cancellationToken = default);
+}
