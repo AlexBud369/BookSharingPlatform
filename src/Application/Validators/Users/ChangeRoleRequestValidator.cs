64e9b3f9e05b961e69978a803be9b5ea6233414f
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-08-05 20:30:13 +0300

    feat: add ChangeRoleRequestValidator - Added ChangeRoleRequestValidator to validate ChangeRoleRequestDto with similar Role validation

---
+++ src/Application/Validators/Users/ChangeRoleRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.Common;
+using Application.DTOs.User;
+using Domain.Constants;
+using FluentValidation;
+using Microsoft.Extensions.Localization;
+
+namespace Application.Validators.Users;
+
+public class ChangeRoleRequestValidator : AbstractValidator<ChangeRoleRequestDto>
+{
+    public ChangeRoleRequestValidator(IStringLocalizer<SharedResources> localizer)
+    {
+        RuleFor(x => x.Role)
+            .NotEmpty().WithMessage(localizer.GetString(SharedResources.RoleRequired))
+            .Must(role => Enum.TryParse<Domain.Enums.UserRole>(role, true, out _))
+            .WithMessage(localizer.GetString(SharedResources.InvalidRole, string.Join(", ", DomainConstants.User.AllowedRoles)));
+    }
+}
