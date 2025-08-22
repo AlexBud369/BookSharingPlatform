b2b0b7aa152727b6b93f743c8983253b6ee02077
AlexBud369 <143994731+AlexBud369@users.noreply.github.com>
2025-08-22 18:12:20 +0300

    feat: add ExpressionExtensions for dynamic predicate combination - Created ExpressionExtensions with And and Or methods to combine Expression<Func<T, bool>> predicates - Supports dynamic query building for future extensibility, although not used in current implementation

---
+++ src/Application/Common/ExpressionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common
+{
+    public static class ExpressionExtensions
+    {
+        public static Expression<Func<T, bool>> And<T>(
+            this Expression<Func<T, bool>> expr1,
+            Expression<Func<T, bool>> expr2)
+        {
+            var parameter = Expression.Parameter(typeof(T));
+            var body = Expression.AndAlso(
+                Expression.Invoke(expr1, parameter),
+                Expression.Invoke(expr2, parameter));
+
+            return Expression.Lambda<Func<T, bool>>(body, parameter);
+        }
+
+        public static Expression<Func<T, bool>> Or<T>(
+            this Expression<Func<T, bool>> expr1,
+            Expression<Func<T, bool>> expr2)
+        {
+            var parameter = Expression.Parameter(typeof(T));
+            var body = Expression.OrElse(
+                Expression.Invoke(expr1, parameter),
+                Expression.Invoke(expr2, parameter));
+
+            return Expression.Lambda<Func<T, bool>>(body, parameter);
+        }
+    }
+}
